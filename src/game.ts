dd3eb281af389af5147540a51ced704c3729aa0d
Marco Catulo <marco.ermes-catulo@alumni.fh-aachen.de>
2022-06-17 14:06:18 +0200

    portalmäßiges halten wieder hinzugefügt. item ablegen ist borked

--- src/game.ts
+++ src/game.ts
@@ -3,30 +3,40 @@ import {scene} from "./scene"
 /// --- Set up a system ---
 
 const Z_OFFSET = 1.5
-const GROUND_HEIGHT = 0.2
+const GROUND_HEIGHT = 2
 
 let holdingItem = false
 let heldItem = ""
+let poweredUp = false
+
+let originalPosition: Vector3 = Vector3.Zero()
+let originalRotation: Quaternion = Quaternion.Zero()
 
 scene.batteryRoboter510.addComponent(
     new OnPointerDown(
         () => {
-            //
+            //          
+            const transform = scene.batteryRoboter510.getComponent(Transform)
             if(!holdingItem) {
+                originalPosition = scene.batteryRoboter510.getComponent(Transform).position
+                originalRotation = scene.batteryRoboter510.getComponent(Transform).rotation
+
                 holdingItem = true
                 heldItem = "battery"
-                scene.batteryRoboter510.getComponent(Transform).translate(new Vector3(0,-1,0))
+                transform.position = Vector3.Zero()
+                transform.rotation = Quaternion.Zero()
+                transform.position.z += Z_OFFSET
+                scene.batteryRoboter510.setParent(Attachable.AVATAR)
             }
-            else {
-
+            else if(heldItem == "battery") {
+                holdingItem = false
+                heldItem = ""
+                scene.batteryRoboter510.setParent(null)
+                transform.position = Vector3.Zero() //TODO: currently moves battery to zero vector. for some reason it doesn't save the original position or the origian position gets overwritten somehow
+                transform.rotation = originalRotation
             }
         },
-        {
-            button: ActionButton.POINTER,
-            showFeedback: true,
-            hoverText: "pick up",
-            distance: 3,
-        }
+        { button: ActionButton.POINTER, showFeedback: true, hoverText: "interact", distance: 3 }
     )
 )
 
@@ -34,23 +44,26 @@ scene.roboter511.addComponent(
     new OnPointerDown(
         () => {
             //
-            if(holdingItem && heldItem == "battery"){
-                heldItem = ""
-                holdingItem = false
-                //scene.batteryRoboter510.getComponent(Transform).translate(new Vector3(0,1,0))
-                scene.roboter511.getComponent(Transform).translate(new Vector3(0,1,0))
-                scene.roboter511.getComponent(Transform).rotation = Quaternion.Zero()
+            if (!poweredUp) {
+                if(holdingItem && heldItem == "battery"){
+                    heldItem = ""
+                    holdingItem = false
+                
+                    scene.batteryRoboter510.setParent(null)
+                    scene.batteryRoboter510.getComponent(Transform).position = Vector3.Zero()
+
+                    scene.roboter511.getComponent(Transform).translate(new Vector3(0,1,0))
+                    scene.roboter511.getComponent(Transform).rotation = Quaternion.Zero()
+                }
+                else {
+
+                }
             }
             else {
 
             }
         },
-        {
-            button: ActionButton.PRIMARY,
-            showFeedback: true,
-            hoverText: "interact",
-            distance: 3,
-        }
+        { button: ActionButton.PRIMARY, showFeedback: true, hoverText: "interact", distance: 4 }
     )
 )
 /*
